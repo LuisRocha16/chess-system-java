08bd12397de149512699c24bed59f0651024f0fe
LuisRocha16 <luis21.filipepi@gmail.com>
2024-06-12 17:57:07 +0100

    First class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,27 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world!");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter row value: ");
+		int row = sc.nextInt();
+		System.out.println("Enter column value: ");
+		int column = sc.nextInt();
+		
+		Position pos = new Position(row, column);
+		
+		System.out.println(pos);
+		
+		sc.close();
 
 	}
 
