7724df493ac560587e833f91cf9ecd7321692d48
LuisRocha16 <luis21.filipepi@gmail.com>
2024-06-12 18:54:49 +0100

    Third class: chess layer and print board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,13 @@
 package application;
 
-import java.util.Locale;
-import java.util.Scanner;
-
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter row value: ");
-		int row = sc.nextInt();
-		System.out.println("Enter column value: ");
-		int column = sc.nextInt();
-		
-		Position pos = new Position(row, column);
-		
-		System.out.println(pos);
-		
-		sc.close();
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
